7597893406a64e9d16417c2a2c6f3b6dbee0d7bf
RJ Smith <rj@rjsmith.me>
2021-12-29 11:32:42 -0500

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hr-notifications
+
+This is a simple package to check for new updates to the Wildcard recruitment form and notify recruiters in Discord of new applications.
